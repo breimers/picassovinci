4f24d5f63f29afbcd5ffc30a427891fbc779716d
breimers <b.a.reimers@gmail.com>
2023-07-21 14:56:09 -0700

    restructured experiment 2, birthed experiment 3

--- training_scripts/experiment_3.py
+++ training_scripts/experiment_3.py
@@ -1,13 +1,11 @@
 import tensorflow as tf
 from tensorflow.keras import layers
 import numpy as np
+import os
 import matplotlib.pyplot as plt
 from PIL import Image
 from IPython.display import display
 from pathlib import Path
-from datasets import load_dataset
-from multiprocessing import cpu_count
-import os
 
 # Set random seed for reproducibility
 tf.random.set_seed(42)
@@ -16,11 +14,11 @@ np.random.seed(42)
 # Define the DCGAN generator model
 def make_generator_model():
     model = tf.keras.Sequential()
-    model.add(layers.Dense(4*4*256, use_bias=False, input_shape=(100,)))
+    model.add(layers.Dense(11 * 14 * 256, use_bias=False, input_shape=(100,)))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
-    model.add(layers.Reshape((4, 4, 256)))
+    model.add(layers.Reshape((11, 14, 256)))
     model.add(layers.Conv2DTranspose(128, (5, 5), strides=(2, 2), padding='same', use_bias=False))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
@@ -29,15 +27,24 @@ def make_generator_model():
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
-    model.add(layers.Conv2DTranspose(3, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+    # Additional Conv2DTranspose layers to achieve target size
+    model.add(layers.Conv2DTranspose(32, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(16, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(3, (5, 5), strides=(2, 1), padding='valid', use_bias=False, activation='tanh'))  # Corrected output shape
 
     return model
+
 
 # Define the DCGAN discriminator model
 def make_discriminator_model():
     model = tf.keras.Sequential()
-    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
-                                     input_shape=[32, 32, 3]))
+    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same', input_shape=[355, 228, 3]))
     model.add(layers.LeakyReLU())
     model.add(layers.Dropout(0.3))
 
@@ -56,7 +63,7 @@ def mutate_latent_space(latent_vector, mutation_rate):
     return mutated_vector
 
 # Define the training loop
-def train_dcgan(generator, discriminator, epochs, batch_size, latent_dim, mutation_rate):
+def train_dcgan(generator, discriminator, dataset, epochs, batch_size, latent_dim, mutation_rate):
     # Define loss functions and optimizers
     cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
     generator_optimizer = tf.keras.optimizers.Adam(1e-4)
@@ -65,10 +72,9 @@ def train_dcgan(generator, discriminator, epochs, batch_size, latent_dim, mutati
     # Track genotype variations
     genotype_variations = []
 
-    dataset = load_dataset("huggan/wikiart", split="train", streaming=True)
-    dataset = dataset.shuffle(seed=42, buffer_size=batch_size)
     @tf.function
     def train_step(images, latent_vector):
+        # Generate noise for fake images
         with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
             # Generate fake images
             generated_images = generator(latent_vector, training=True)
@@ -89,20 +95,8 @@ def train_dcgan(generator, discriminator, epochs, batch_size, latent_dim, mutati
         return gen_loss, disc_loss
 
     for epoch in range(epochs):
-        try:
-            dataset.set_epoch(epoch)
-            epoch_dataset = [next(iter(dataset)) for i in range(batch_size)]
-            images = []
-            img_count = 0
-            for image in epoch_dataset:
-                img_count += 1
-                image = image["image"]
-                image = np.asarray(image)
-                image = np.resize(image, (32, 32, 3))
-                images.append(image)
+        for images in dataset:
             # Normalize images to [-1, 1]
-            images = (np.array(images) - 0.5) * 2
-
             # Generate random latent vectors
             latent_vectors = tf.random.normal([batch_size, latent_dim])
 
@@ -115,39 +109,63 @@ def train_dcgan(generator, discriminator, epochs, batch_size, latent_dim, mutati
 
             # Track genotype variations
             genotype_variations.append(mutated_latent_vectors)
-            
-            print(f'Epoch {epoch + 1}/{epochs}, Generator Loss: {gen_loss:.4f}, Discriminator Loss: {disc_loss:.4f}, Mutator Loss: {mutated_gen_loss:.4f}, Seeker Loss: {mutated_disc_loss:.4f}')
 
-            # Display progress every few epochs
-            if (epoch + 1) % 5 == 0:
-                # Generate a sample image every 10 epochs
-                sample_latent_vector = tf.random.normal([1, latent_dim])
-                generated_image = generator(sample_latent_vector, training=False)
-                generated_image = (generated_image[0] + 1.0) / 2.0  # De-normalize to [0, 1]
-                plt.imshow(generated_image)
-                plt.axis('off')
-                fig = plt.gcf()
-                fig.savefig(f"generator_model/{Path(__file__).stem}/{epoch + 1}.png")
-        except Exception as e:
-            print(e)
-            continue
+        # Display progress every epoch
+        print(f'Epoch {epoch + 1}/{epochs}, Generator Loss: {gen_loss:.4f}, Discriminator Loss: {disc_loss:.4f}, Mut. Generator Loss: {gen_loss:.4f}, Mut. Discriminator Loss: {disc_loss:.4f}')
+
+        # Generate a sample image every 5 epochs
+        if (epoch + 1) % 5 == 0:
+            sample_latent_vector = tf.random.normal([1, latent_dim])
+            generated_image = generator(sample_latent_vector, training=False)
+            generated_image = (generated_image[0] + 1.0) / 2.0  # De-normalize to [0, 1]
+            plt.imshow(generated_image)
+            plt.axis('off')
+            fig = plt.gcf()
+            fig.savefig(f"generator_model/{Path(__file__).stem}/{epoch + 1}.png")
 
     return genotype_variations
 
-# Set hyperparameters
+# Prepare the dataset (folder containing JPEG images)
+if not os.path.exists(f"generator_model/{Path(__file__).stem}"):
+    os.mkdir(f"generator_model/{Path(__file__).stem}")
+
 epochs = 50
-batch_size = 20
+batch_size = 32
 latent_dim = 100
-mutation_rate = 0.2
+mutation_rate = 0.12
+
+def preprocess_image(image, label):
+    # Resize and normalize the images
+    image = tf.image.resize(image, (355, 228))
+    image = (image - 127.5) / 127.5  # Normalize to [-1, 1]
+    return image
+
+# Prepare the dataset (folder containing JPEG images)
+data_path = '/home/breimers/Pictures/img_align_celeba/train/celeb'
+image_files = tf.data.Dataset.list_files(data_path + '/*.jpg', shuffle=True)
+
+# Define a function to read and preprocess the image files
+def read_and_preprocess_image(file_path):
+    image = tf.io.read_file(file_path)
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = preprocess_image(image, label=None)  # We don't need labels
+    print("Image shape:", image.shape)  # Add this line to print the image shape
+    return image
+
+# Map the image file paths to the read_and_preprocess_image function
+train_dataset = image_files.map(read_and_preprocess_image)
+
+# Shuffle and batch the dataset
+train_dataset = train_dataset.shuffle(buffer_size=1000).batch(batch_size).prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+
+print("Built Dataset")
+# Set hyperparameters
 
 # Create the generator and discriminator models
 generator = make_generator_model()
 discriminator = make_discriminator_model()
 
-if not os.path.exists(f"generator_model/{Path(__file__).stem}"):
-    os.mkdir(f"generator_model/{Path(__file__).stem}")
-
 # Train the DCGAN with latent space mutation and track genotype variations
-genotype_variations = train_dcgan(generator, discriminator, epochs, batch_size, latent_dim, mutation_rate)
+genotype_variations = train_dcgan(generator, discriminator, train_dataset, epochs, batch_size, latent_dim, mutation_rate)
 generator.save(f"generator_model/{Path(__file__).stem}")
 np.save(f"generator_model/{Path(__file__).stem}/genotype_variations.npy", genotype_variations)
